7326ad1341cf526a196fa1c43a85e11f5f82b391
Matias Scherer Lunkes <mslunkes@outlook.com>
2024-10-15 19:36:50 -0300

    adicionado mais seções ate o footer

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,13 @@ const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin')
 
 function styles() {
-    return gulp.src('.src/styles/*.scss')
+    return gulp.src('./src/styles/*.scss')
         .pipe(sass({outputStyle: 'compressed'}))
         .pipe(gulp.dest('./dist/css'));
 }
 
 function images(){
-    return gulp.src('.src/images/**/*')
+    return gulp.src('./src/images/**/*')
         .pipe(imagemin())
         .pipe(gulp.dest('./dist/images'));
 }
